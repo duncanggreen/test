c619113cfce33249a30bdd6679ff4d0aab357e4c
Duncan Green <dggreen3@alaska.edu>
2018-09-25 16:20:40 -0800

    edited x, created y

--- new_test_script.R
+++ new_test_script.R
@@ -1,2 +1,9 @@
 ## New script
-x <- c(1,2,3,4,5,6,7)
+x <- c(1,2,3,4,5,6)
+
+# Click on 'Git' in the upper right
+# Click 'staged' box for everything you want to commit together
+# CLick commit button
+# Add comment, click Push
+
+y <- "cats"
